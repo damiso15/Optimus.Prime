11dbf8a0f11955c119abac1e9432b7e9a2975376
damiso15 <damiso15@yahoo.com>
2020-03-25 08:22:06 +0100

    Python Program to Calculate the Area of a Circle

---
+++ 1.AreaCalculator.py
@@ -0,0 +1,7 @@
+# This is a Python PRogram that Calculate the Area of a Circle using Radius
+
+Pi = 3.14
+R = float(input("Please Enter the Radius of your Circle: "))
+Area = Pi * R * R
+
+print("The Area of your Circle = %.2f" %Area)
